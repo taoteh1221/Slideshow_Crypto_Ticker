06403808e2060ae8d69b178ebb0873017985c9ae
Michael Kilday <mike@dragonfrugal.com>
2019-10-15 23:56:12 -0400

    Project pages maintenance stuff
    Project pages maintenance stuff

---
+++ dfd-crypto-ticker/Misc.Docs.Etc/Developing-maintaining/scripts/Git-Mirror-Ticker-App-Github-To-Sourceforge.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#enter your build directory
+cd /home/taoteh1221/Build/
+
+#uncomment first run only
+#git clone --bare --mirror https://github.com/taoteh1221/DFD_Crypto_Ticker.git
+
+#enter your repo directory
+cd DFD_Crypto_Ticker.git
+
+#uncomment first run only
+#git remote add sourceforge ssh://taoteh1221@git.code.sf.net/p/dfd-crypto-ticker/code
+
+#uncomment first run only
+#git config remote.sourceforge.mirror true
+
+
+########################################
+#run every time to mirror original repo#
+########################################
+
+git fetch --quiet origin
+git push --quiet sourceforge
+
+git config alias.update-mirror '!git fetch -q origin && git push -q sourceforge'
+git update-mirror
+
+echo 'Sourceforge Mirror updated'
